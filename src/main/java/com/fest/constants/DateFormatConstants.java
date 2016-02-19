ccafa08d66dc62668fc928a0d646882b62f372a1
wilsonpenagos <sheniori@gmail.com>
2016-02-19 17:32:13 -0500

    Constants
    Main constants file

---
+++ src/main/java/com/fest/constants/DateFormatConstants.java
@@ -0,0 +1,8 @@
+package com.fest.constants;
+
+public final class DateFormatConstants {
+	
+	public static final String MAIN_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+	public static final String SECONDARY_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+
+}
